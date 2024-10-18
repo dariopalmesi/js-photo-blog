59b0b05ddfc694fcf41cba14b469cd50d78405d5
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-18 12:33:14 +0200

    upgrade html css js

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,14 @@
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
 .then(response => {
+    const posts = response.data
+    console.log(posts);
     console.log(response);
+
+    posts.forEach(post => {
+        console.log(post);
+        
+    })
     
-})
+  }).catch(error => console.error(error))
+
+   
