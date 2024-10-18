a63b4a586dedd74c7636f120c23be988e6c0e0be
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-18 16:21:14 +0200

    upgrade commit

--- assets/main.js
+++ assets/main.js
@@ -6,26 +6,32 @@ axios.get('https:jsonplaceholder.typicode.com/photos?_limit=6')
         console.log(posts);
         console.log(response);
 
+
+
         const postsEl = document.getElementById('posts')
 
-         posts.forEach(post => {
-             console.log(post);
-             const {url, title} = post
-             let markup = `
-             <div class="card col-4 mt-5 ">
-                     <img src="${url}" class="card-img-top border" alt="">
-                     <img src="./assets/img/pin.svg" class="pin" alt="">
-                     <div class="card-body">
-                         <p class="card-text">
-                             ${title}
-                         </p>
-                     </div>
-                 </div>
-             `
+        posts.forEach(post => {
+            console.log(post);
+            const { url, title } = post
+            let markup = `
+               <div class="card col-4 mt-5">
+                       <img src="${url}" class="card-img-top border" alt="">
+                       <img src="./assets/img/pin.svg" class="pin" alt="">
+                       <div class="card-body">
+                           <p class="card-text">
+                               ${title}
+                           </p>
+                       </div>
+                   </div>
+               `
 
-             postsEl.innerHTML += markup
-         })
+            postsEl.innerHTML += markup
+        })
 
     }).catch(error => console.error(error))
 
 
+const postsCorrect = posts.map((character) => {
+    return character.charAt(0).toLocaleUpperCase()
+})
+console.log(postsCorrect);
