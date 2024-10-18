f3f0af1be9fc6d6eb9030a1e687da43102113327
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-18 13:06:35 +0200

    upgrade commit

--- assets/main.js
+++ assets/main.js
@@ -1,14 +1,31 @@
+
+
 axios.get('https://jsonplaceholder.typicode.com/photos?_limit=6')
-.then(response => {
-    const posts = response.data
-    console.log(posts);
-    console.log(response);
+    .then(response => {
+        const posts = response.data
+        console.log(posts);
+        console.log(response);
 
-    posts.forEach(post => {
-        console.log(post);
-        
-    })
-    
-  }).catch(error => console.error(error))
+        const postsEl = document.getElementById('posts')
 
-   
+        // posts.forEach(post => {
+        //     console.log(post);
+        //     const [url, title] = post
+        //     let markup = `
+        //     <div class="card col-4 ">
+        //             <img src="${url}" class="card-img-top border" alt="">
+        //             <img src="./assets/img/pin.svg" class="pin" alt="">
+        //             <div class="card-body">
+        //                 <p class="card-text">
+        //                     ${title}
+        //                 </p>
+        //             </div>
+        //         </div>
+        //     `
+
+        //     postsEl.innerHTML += markup
+        // })
+
+    }).catch(error => console.error(error))
+
+
