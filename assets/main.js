e3776a2ddf7392c63b87c3961fd057bc371953da
dariopalmesi <dariopalmesi88@gmail.com>
2024-10-21 16:30:58 +0200

    upgrade commit

--- assets/main.js
+++ assets/main.js
@@ -1,5 +1,5 @@
 
-axios.get('https:jsonplaceholder.typicode.com/photos?_limit=6')
+axios.get("https://jsonplaceholder.typicode.com/photos?_limit=6")
     .then(response => {
         const posts = response.data
         console.log(posts);
@@ -14,9 +14,9 @@ axios.get('https:jsonplaceholder.typicode.com/photos?_limit=6')
             const { url, title } = post
             let markup = `
                <div class=" card col-lg-4 col-sm-12 col-md-6 mt-5">
-                       <img src="${url}" class="card-img-top border" alt="">
+                       <img src="${url}" class="card-img-top border overlay-image" alt="">
                        <img src="./assets/img/pin.svg" class="pin" alt="">
-                       <div class="card-body ">
+                       <div class="card-body overlay">
                            <p class="card-text">
                                ${title}
                            </p>
